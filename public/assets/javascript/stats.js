885a798296e7e1a9f5b31ef1e0c53396bae14d33
Kathleen Souders <ksouders@knights.ucf.edu>
2018-06-02 09:34:14 -0400

    new stats

--- public/assets/javascript/stats.js
+++ public/assets/javascript/stats.js
@@ -1,10 +1,10 @@
-var myChart = document.getElementById('myChart').getContext('2d');
+var chart1 = document.getElementById('chart1').getContext('2d');
 
-var mostPlanesChart = new Chart (myChart,{
+var mostPlanesChart = new Chart (chart1,{
     type: 'pie',
     data: {
         labels: ['US', 'China', 'Canada', 'Germany', 'UK', 'France', 'Japan', 'Brazil', 'Spain', 'India'],
-        datasets[{
+        datasets: [{
             label: 'Top 10 Countries with Most Planes in the Air',
             data: [
                 9054478,
@@ -17,8 +17,31 @@ var mostPlanesChart = new Chart (myChart,{
                 647753,
                 616893,
                 592292,
+            ]
+            
+        }],
+    },
+    options: {}
+});
 
-
+var chart2 = document.getElementById('chart2').getContext('2d');
+var mostPassengersChart = new Chart (chart2,{
+    type: 'pie',
+    data: {
+        labels: ['US', 'China', 'UK', 'Germany', 'Japan', 'Ireland', 'Brazil', 'India', 'Turkey', 'Indonesia'],
+        datasets: [{
+            label: 'Top 10 Countries with Most Passengers in the Air',
+            data: [
+                798230000,
+                436183969,
+                131449680,
+                115540886,
+                113762000,
+                113144501,
+                102039359,
+                98927860,
+                96604665,
+                88685767,
             ]
             
         }],
