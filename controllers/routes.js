a986dbddf06f1b281cd8ac751f47d8e239748bdf
Kathleen Souders <ksouders@knights.ucf.edu>
2018-06-06 18:30:16 -0400

    styling

--- controllers/routes.js
+++ controllers/routes.js
@@ -16,6 +16,19 @@ var db = require("../models");
      
     });
 
+    router.get("/airports/search", function (req, res) {
+
+        var type = req.query.type;  
+        var airportSearch =req.query.airport_search; 
+        db.Airport.findAll({
+            where: { [type]: airportSearch}
+         }).then(function(airports) {
+           res.json(airports);
+         });
+       
+        
+       });
+
     
 
 module.exports = router;
